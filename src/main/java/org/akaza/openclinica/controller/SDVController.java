655119d35e2b98cc5b9a3778063dfb99a56a706b
Adam <syahnetwork@gmail.com>
2020-03-20 22:58:14 +0700

    reformat code

--- src/main/java/org/akaza/openclinica/controller/SDVController.java
+++ src/main/java/org/akaza/openclinica/controller/SDVController.java
@@ -198,7 +198,7 @@ public class SDVController {
         StudyInfoPanel panel = new StudyInfoPanel();
         panel.reset();
         request.getSession().setAttribute("panel", panel);
-        request.setAttribute("iconInfoShownSDV",true);//adam
+        request.setAttribute("iconInfoShownSDV", true);//adam
 
 
         ModelMap gridMap = new ModelMap();
